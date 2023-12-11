60620ea709bb1d4a28e2245a07803d4c0559006d
Anik Das <onikd08@gmail.com>
2023-12-11 21:51:56 +0200

    Add email as query in /booking getAPI to load user specific bookings

--- index.js
+++ index.js
@@ -42,7 +42,11 @@ async function run() {
 
     // getAPI for all bookings
     app.get("/bookings", async (req, res) => {
-      const result = await bookingCollection.find({}).toArray();
+      let query = {};
+      if (req.query.email) {
+        query = { email: req.query.email };
+      }
+      const result = await bookingCollection.find(query).toArray();
       res.send(result);
     });
 
