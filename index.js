f00cbe92e023be316576bb1aa76f10c742901b26
Anik Das <onikd08@gmail.com>
2023-12-11 17:36:50 +0200

    Add mongoDB connection

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient, ServerApiVersion } from "mongodb";
+import "dotenv/config";
 
 const app = express();
 const port = process.env.PORT || 8000;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@phero.kuclg9t.mongodb.net/?retryWrites=true&w=majority`;
 
 //middlewares
 app.use(cors());
 app.use(express.json());
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
